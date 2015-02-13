1120b955dbf040529c64c5426cc65512dab3dce0
Matthias Diener <mdiener@inf.ufrgs.br>
2015-02-13 18:16:48 -0200

    recompile if necessary

--- run.sh
+++ run.sh
@@ -2,5 +2,10 @@
 
 set -o errexit -o nounset -o pipefail -o errtrace -o posix
 
+# directory of this script
+DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# recompile pintool if necessary
+(cd $DIR; make -q || make)
 
 pin -xyzzy -enable_vsm 0 -t obj-*/cache_sim.so -- ${@}
